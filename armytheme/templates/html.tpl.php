b93bfede94c95240f67210358ae367850b77ec38
ArmyIM <armywebdev@gmail.com>
2016-09-21 14:49:51 +1000

    Added pushy menu. Added IE7 basic support.

--- armytheme/templates/html.tpl.php
+++ armytheme/templates/html.tpl.php
@@ -54,6 +54,7 @@
  * @ingroup templates
  */
 ?><!DOCTYPE html>
+<!--[if lt IE 7]><html class="lt-ie7" lang="en"><![endif]-->
 <!--[if IE 8]><html class="lt-ie8" lang="en"><![endif]-->
 <!--[if gt IE 8]><!-->
 <html lang="en">
@@ -74,24 +75,33 @@
 
   <!-- HTML5 element support for IE6-8 -->
   <!--[if lt IE 9]>
-    <script src="sites/all/themes/armytheme/dist/js/html5shiv.min.js"></script>
+    <script type="text/javascript" src="sites/all/themes/armytheme/dist/js/html5shiv.min.js"></script>
   <![endif]-->
 
  <?php print $scripts; ?>
-<!--[if gt IE 8]><!-->
-<script src="sites/all/themes/armytheme/dist/js/pushy.min.js"></script>
-<!--<![endif]-->
 
-</head>
+</head> 
 
 <body<?php print $body_attributes; ?>>
+
+<!--[if lt IE 7]>
+<div class="message-outdated">
+    <p>You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    </div>
+<![endif]-->
+
   <div id="skip-link">
     <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
   </div>
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
-  
+
+
+<!--[if gt IE 8]><!-->
+<script type="text/javascript" src="sites/all/themes/armytheme/dist/js/pushy.min.js"></script>
+<!--<![endif]-->
+
 <!--[if gt IE 8]><!-->
   <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-57c9545832e949a6"></script>
 <!--<![endif]-->
