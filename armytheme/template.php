1a5b2b30c9f2355f1a066368e101c0a60c381b1a
ArmyIM <armywebdev@gmail.com>
2016-09-21 15:28:08 +1000

    Updated mobile menu.

--- armytheme/template.php
+++ armytheme/template.php
@@ -12,13 +12,11 @@ function armytheme_preprocess_page(&$vars, $hook)
     }
 }
 
-
 function armytheme_js_alter(&$javascript) {
     $javascript['misc/jquery.js']['data'] = drupal_get_path('theme', 'armytheme') .
     '/dist/js/jquery-1.12.4.min.js';
     $javascript['misc/jquery.js']['version'] = '1.7.2';
 }
-
 
 /**
  * Bootstrap theme wrapper function for the primary menu links.
@@ -26,7 +24,6 @@ function armytheme_js_alter(&$javascript) {
 function armytheme_menu_tree__primary(&$variables) {
   return '<ul class="menu nav">' . $variables['tree'] . '</ul>';
 }
-
 
 function armytheme_menu_link__main_menu($variables)
 {
@@ -49,8 +46,8 @@ function armytheme_menu_link__main_menu($variables)
     } else {
       unset($element['#below']['#theme_wrappers']);
       $sub_menu = '<ul class="dropdown-menu-nav">' . drupal_render($element['#below']) . '</ul>';
-      //$element['#title'] .= ' <span class="caret"></span>';
-      $element['#attributes']['class'][] = 'dropdown';
+      $element['#title'] .= ' <span class="caret"></span>';
+      $element['#attributes']['class'][] = 'dropdown pushy-submenu';
       $element['#localized_options']['html'] = TRUE;
       $element['#localized_options']['attributes']['class'][] = 'dropdown-toggle';
       $element['#localized_options']['attributes']['data-toggle'] = 'dropdown';
@@ -61,6 +58,20 @@ function armytheme_menu_link__main_menu($variables)
   }
   $output = l($element['#title'], $element['#href'], $element['#localized_options']);
   return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
+}
+
+
+/**
+ * Add mobile nav calsses
+ */
+
+
+function armytheme_menu_link__menu_block_6(array $variables) {
+  // Add class for li.
+  $variables['element']['#attributes']['class'][] = 'blah-' . $variables['element']['#original_link']['mlid'];
+  // Add class for a.
+ $variables['element']['#localized_options']['attributes']['class'][] = 'menu-' . $variables['element']['#original_link']['mlid'];
+  return armytheme_menu_link__menu_block__6($variables);
 }
 
 
