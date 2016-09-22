3021f5a401ab46a5f1a5ad7b2d06b5711832e2ea
ArmyIM <armywebdev@gmail.com>
2016-09-23 00:35:25 +1000

    Combined bootstrap JS with other scripts.

--- armytheme/templates/html.tpl.php
+++ armytheme/templates/html.tpl.php
@@ -30,7 +30,7 @@
   <![endif]-->
 <?php print $scripts; ?>
 </head> 
-<body<?php print $body_attributes; ?>>
+<body <?php print $body_attributes; ?>>
 <!--[if lt IE 7]>
 <div class="message-outdated">
     <p>You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
