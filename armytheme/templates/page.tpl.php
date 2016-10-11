3faa2e589d2b1d7b846519e687bb08e7f4e40b9f
ArmyIM <armywebdev@gmail.com>
2016-10-11 12:07:14 +1100

    Updated styles.

--- armytheme/templates/page.tpl.php
+++ armytheme/templates/page.tpl.php
@@ -74,7 +74,7 @@
           <a id="main-content"></a>
 
         <?php print render($title_prefix); ?>
-          <?php if ($title): ?>
+          <?php if (!$page && !empty($title)): ?>
             <h1 class="page-header">
               <?php print $title; ?>
             </h1>
