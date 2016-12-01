d244f543155801e289263faa8873b3c6395c0483
ArmyIM <armywebdev@gmail.com>
2016-12-01 13:43:40 +1100

    Accessibility fix: Added text to global search label.

--- bootstrap/templates/bootstrap/bootstrap-search-form-wrapper.func.php
+++ bootstrap/templates/bootstrap/bootstrap-search-form-wrapper.func.php
@@ -11,7 +11,7 @@
  */
 function bootstrap_bootstrap_search_form_wrapper($variables) {
   $output = '<div role="search" class="input-group">';
-  $output .= '<label class="sr-only" for="search_block_form" aria-label="Search for:"></label>';
+  $output .= '<label class="sr-only" for="search_block_form">Search for:</label>';
   $output .= $variables['element']['#children'];
   $output .= '<span class="input-group-btn">';
   $output .= '<button aria-label="Perform search." type="submit" class="btn btn-primary"><i class="fa fa-search"></i></button>';
