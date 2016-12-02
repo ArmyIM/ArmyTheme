da871db14b1c352e8606a23e34c8b052d67563c7
ArmyIM <armywebdev@gmail.com>
2016-12-02 11:10:21 +1100

    no message

--- armytheme/template.php
+++ armytheme/template.php
@@ -10,15 +10,11 @@ function armytheme_preprocess_page(&$vars, $hook)
 }
 
 
-
 function armytheme_preprocess_html(&$vars) {
   if (isset($node->field_grid_template_[LANGUAGE_NONE][0]['value']) && $node->field_grid_template_[LANGUAGE_NONE][0]['value'] == 1 ) :
     $vars['classes_array'][] = 'grid-layout';
   endif;
 }
-
-
-
 
 
 function armytheme_menu_tree__primary(&$variables) {
