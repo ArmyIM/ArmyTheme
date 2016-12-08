81264e3ab9eaa4010d1aaad5f67359108a36dbd9
ArmyIM <armywebdev@gmail.com>
2016-12-08 22:06:03 +1100

    Update to img gallery tpl

--- armytheme/templates/views-view-unformatted--army-nodes-image-gallery.tpl.php
+++ armytheme/templates/views-view-unformatted--army-nodes-image-gallery.tpl.php
@@ -1,13 +1,11 @@
 <?php if (!empty($title)): ?>
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
-
 <div class="gallery" data-featherlight-gallery data-featherlight-filter="a">
 <?php foreach ($rows as $id => $row): ?>
-  <div <?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
+  <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
     <?php print $row; ?>
   </div>
-  <?php if ($id == 2 || $id == 5 || $id == 8 || $id == 11): ?><div class="clearfix""></div><?php endif; ?>
 <?php endforeach; ?>
 </div>
 
