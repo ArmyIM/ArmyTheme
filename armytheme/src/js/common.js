f122bd4bd609ddb0ac77cc68904353057ae1997d
ArmyIM <armywebdev@gmail.com>
2016-12-08 21:35:18 +1100

    Updates!

--- armytheme/src/js/common.js
+++ armytheme/src/js/common.js
@@ -1,7 +1,24 @@
 jQuery(document).ready(function($) {
 
+  var $slider = $('#feature-slides')
+        .on('init', function(slick) {
+            $('#feature-slides').fadeIn(1000);
+        })
+  .slick({
+                  focusOnSelect: true,
+            autoPlay: true,
+            lazyLoad: 'ondemand',
+            dots: true,
+            centerMode: true,
+            pauseOnDotsHover: true,
+            infinite: true,
+            speed: 300,
+            arrows:true,
+            height: 450,
+            centerPadding: 0,
+            appendDots: $('#dots-nav')
+  });
 
-$(function() {
   var viewPortWidth = $(window).width();
 
   $(window).scroll(function(event) {
@@ -19,14 +36,22 @@ $(function() {
     $('html, body').animate({scrollTop : 0 }, 600);
     event.preventDefault();
   });
-});
+
 
   $('#secondary li.expanded a').addClass('disabled');
 
   if($(".feature-img").hasClass("img-sm")) 
      $("body").addClass("has-img-sm");
 
-    $('.gallery').featherlightGallery();
-
+    $('.gallery').featherlightGallery({
+            gallery: {
+                fadeIn: 300,
+                fadeOut: 300,
+                next: 'next »',
+                previous: '« previous'
+            },
+            openSpeed:    300,
+            closeSpeed:   300
+        });
 
 });
---
+++ armytheme/templates/views-view-fields--army-nodes-image-gallery.tpl
@@ -0,0 +1,8 @@
+<div class="gallery" data-featherlight-gallery data-featherlight-filter="a">
+ <?php if ($rows): ?>
+      <?php print $rows; ?>
+     <?php endif; ?>
+</div>
+
+
+<div id="dots-nav" class="container"></div>
