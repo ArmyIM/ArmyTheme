8958d5fe0fcecaac98dd4deb79cd3863e775fbc0
ArmyIM <armywebdev@gmail.com>
2016-12-08 15:08:31 +1100

    Category grid tpl

--- armytheme/templates/views-view-unformatted--category-grid-layout.tpl.php
+++ armytheme/templates/views-view-unformatted--category-grid-layout.tpl.php
@@ -1,43 +1,9 @@
-<?php
-/*
-* Target
-* - I want to print custom code after every 4 row and keep the odd/even sequence
-* Solution:
-* - Customize $classes_array[$id] variable
-* Note:
-* - Computers count like: 0,1,2,3... Not like we, the humans: 1,2,3,4...
-* Configuration:  
-* - I have view with 10 rows
-* - My $classes_array[$id] is "views-row-even row" ("views-row-odd row")
-* Logic:
-* - Untip row with ID 0, 1, 2, 3 print "odd, even, odd, even."
-* - Insert custom row together with row 4 (ID 3) with class="views-row-odd row"
-* - Since row 5 (ID 4)  change default order and start as "even, odd, even, odd..."     
-*/
-?>
+<?php if (!empty($title)): ?>
+  <h3><?php print $title; ?></h3>
+<?php endif; ?>
 <?php foreach ($rows as $id => $row): ?>
-  <div class="<?php
-  switch ($id) {
-    case 1:
-    case 3:
-    case 4:
-    case 6:
-    case 8:
-    //add more IDs if you have more rows than 10 in your views
-      $myclasses = 'views-row-even row';
-    break;
-    
-    default:
-      $myclasses = 'views-row-odd row';
-  }  
-  print $myclasses; ?>"><?php print $row; ?></div>
-  <?php
-  // print custom code after 4th row
-  if ($id == 2): ?>
-    <div class="views-row-odd row">
-      custom code goes here
-    </div>
-  <?php endif;
-  //end of custom code
-  ?>
+  <li <?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
+    <?php print $row; ?>
+  </div>
+  <?php if ($id == 2 || $id == 5 || $id == 8 || $id == 11): ?><div class="clearfix""></div><?php endif; ?>
 <?php endforeach; ?>
