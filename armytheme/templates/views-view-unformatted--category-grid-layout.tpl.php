d1890cd8647ae13c7c6314a6a8491e31809d64ce
ArmyIM <armywebdev@gmail.com>
2016-12-08 16:34:13 +1100

    Incorrect markup

--- armytheme/templates/views-view-unformatted--category-grid-layout.tpl.php
+++ armytheme/templates/views-view-unformatted--category-grid-layout.tpl.php
@@ -2,7 +2,7 @@
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
 <?php foreach ($rows as $id => $row): ?>
-  <duv <?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
+  <div <?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
     <?php print $row; ?>
   </div>
   <?php if ($id == 2 || $id == 5 || $id == 8 || $id == 11): ?><div class="clearfix""></div><?php endif; ?>
