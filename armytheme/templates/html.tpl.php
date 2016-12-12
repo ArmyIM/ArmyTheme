59456efa2f95309ba997df7fb2a441219085d13c
ArmyIM <armywebdev@gmail.com>
2016-12-12 16:45:10 +1100

    removed addthis

--- armytheme/templates/html.tpl.php
+++ armytheme/templates/html.tpl.php
@@ -39,6 +39,5 @@
   <?php print $page; ?>
   <?php print $page_bottom; ?>
 </div>
-<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-584a8b48b7cba381"></script>
 </body>
 </html>
