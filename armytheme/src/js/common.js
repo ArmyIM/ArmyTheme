40bec50809fcba7900e9d778b1cc0bf5a54ad330
ArmyIM <armywebdev@gmail.com>
2016-12-13 19:35:53 +1100

    JS common

--- armytheme/src/js/common.js
+++ armytheme/src/js/common.js
@@ -47,8 +47,19 @@
         navbars: 
             [
                 {
+                               position: top
+                },
+                {
                                position: top,
                                content: ["searchfield"]
+                },
+                {
+                               position: bottom,
+                               content: [
+                                  "<a class='fa fa-facebook' href='https://www.facebook.com/TheAustralianArmy'></a>",
+                                  "<a class='fa fa-twitter' href='http://twitter.com/AustralianArmy'></a>",
+                                  "<a class='fa fa-youtube' href='http://www.youtube.com/user/AustralianArmyHQ'></a>"
+                               ]
                 }
             ]
         });
