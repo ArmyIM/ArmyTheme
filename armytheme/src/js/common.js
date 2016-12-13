d8119fd6a5da6da53724888307e487a86ccff412
ArmyIM <armywebdev@gmail.com>
2016-12-13 16:31:50 +1100

    Upated common js

--- armytheme/src/js/common.js
+++ armytheme/src/js/common.js
@@ -31,22 +31,31 @@
     var $menu = $('#mobile-nav');
 
     $menu.mmenu({
-    slidingSubmenus:    true,
-    searchfield:        true,
-    toggleText:         '<span class="sr-only">toggle submenu</span>',
-    extensions:         ["effect-menu-slide", "effect-listitems-slide","theme-dark"],
-    dragOpen:           true,
-    header: {
-                        add         : true,
-                        update      : true,
-                        title       : 'Navigation'
+        slidingSubmenus:        true,
+        toggleText:             '<span class="sr-only">toggle submenu</span>',
+        extensions:             ["effect-menu-slide", "effect-listitems-slide","theme-dark"],
+        dragOpen:               true,
+        setSelected:        
+            {
+                                hover: true,
+                                parent: true
+            },     
+        searchfield: 
+            {
+                                resultsPanel: true,
+                                clear: true
+
             },
-    
-    });
-
-              
-});
-
+     
+        navbars: 
+            [
+                {
+                               position: top,
+                               content: [searchfield]
+                }
+            ]
+        });
+   });
 
  var gallery = document.getElementsByClassName('gallery');
     var mc = new Hammer(gallery);
