93d3a6a9cd0d8e3df7a6122ad695c985136c82fa
ArmyIM <armywebdev@gmail.com>
2016-12-14 12:26:31 +1100

    no message

--- armytheme/templates/views-view-unformatted--slideshow--block.tpl.php
+++ armytheme/templates/views-view-unformatted--slideshow--block.tpl.php
@@ -1,21 +1,17 @@
-<?php foreach ($rows as $id => $row): ?>
-  <div id="feature-slides" style="display:none;">
-    <?php print $row; ?>
-  </div>
-<?php endforeach; ?>
-
-
-
-
+<div id="feature-slides" style="display:none;">
+ <?php if ($rows): ?>
+      <?php print $rows; ?>
+     <?php endif; ?>
+</div>
 <div id="dots-nav" class="container"></div>
 <script>
 jQuery(document).ready(function($) {
   var $slider = $('#feature-slides')
-  		.on('init', function(slick) {
+      .on('init', function(slick) {
             $('#feature-slides').fadeIn(1000);
         })
   .slick({
-			      focusOnSelect: true,
+            focusOnSelect: true,
             autoPlay: true,
             lazyLoad: 'ondemand',
             dots: true,
